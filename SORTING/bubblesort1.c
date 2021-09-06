717be8fbe37b00327e6e9a661497217474122c86
udhayamoorthi04 <udhayamoorthi60@gmail.com>
2021-09-06 09:51:18 +0530

    updates

--- SORTING/bubblesort1.c
+++ SORTING/bubblesort1.c
@@ -4,23 +4,23 @@
 // the bubble sort scanning the list and excange the adjacent if they are out of order with respect to each other.
 
 #include<stdio.h>
-#define MAX 100
+#define MAX 100// all lines that start with # are prepocessed by preprocessor
 int main()
 {
 	int arr[MAX],i,k,j,n,temp,xchanges;
 	
-	printf("Enter the num of ele:");
+	printf("Enter the num of ele:");// program has to enter the value
 	scanf("%d",&n);
 
 	/*bumble sort*/
-	for(i=0;i<n-1;i++)
+	for(i=0;i<n-1;i++)//value n-1 
 	{
 		xchanges =0;
-		for(j=0;j<n-1-i;j++)
+		for(j=0;j<n-1-i;j++)//value n-1-i
 		{
-			if(arr[j]>arr[j+1])
+			if(arr[j]>arr[j+1])//a[j] will be compared with arr[j+1]
 			{
-				temp=arr[j];
+				temp=arr[j];/* if arr[j]>arr[j+1] then they will be swapped*/
 				arr[j]=arr[j+1];
 				arr[j+1]=temp;
 				xchanges++;
@@ -29,7 +29,7 @@ int main()
 		if(xchanges==0)/*if lis is sorted*/
 			break;
 	}
-		printf("sorted list is:\n");
+		printf("sorted list is:\n");//this printf for print the output
 	for(i=0;i<n;i++)
 		printf("%d",arr[i]);
 
--- SORTING/insertion.c
+++ SORTING/insertion.c
@@ -4,16 +4,16 @@
 // the insertion sort proceeds by inserting each ele at the proper place in a sorted list.
 
 #include<stdio.h>
-#define MAX 100
+#define MAX 100//all lines that start with # are processed by preprocessor
 int main()
 {
 	int arr[MAX],i,k,j,n;
 	
-	printf("Enter the num of ele:");
+	printf("Enter the num of ele:");//programmer has to enter the value
 	scanf("%d",&n);
 
 	/*insertion sort*/
-	for(i=0;i<n;i++)
+	for(i=0;i<n;i++)//this is for the input suppose 10,30,20,4,5
 	{
 		printf("enter ele %d:",i+1);
 		scanf("%d",&arr[i]);
@@ -24,9 +24,9 @@ int main()
 		for(j=i-1;j>=0 && k<arr[j];j--)
 
 			arr[j+1]=arr[j];
-		arr[j+1]=k;
+		arr[j+1]=k;//the element to be inserted (arr[i] is stored in k
 	}
-	printf("sorted list is:\n");
+	printf("sorted list is:\n");//this is for output 4 5 10 20 30
 	for(i=0;i<n;i++)
 		printf("%d",arr[i]);
 
