f2b7916417d98130f655842d3e529df92d76b93f
udhayamoorthi04 <udhayamoorthi60@gmail.com>
2021-09-06 07:45:40 +0530

    vfork()

---
+++ VFork/vf1.c
@@ -0,0 +1,18 @@
+/*modifies any data other than a variable of types pid_t
+ * return from the function in which vfork was called\
+ */
+#include<stdio.h>
+
+int main()
+{
+	if(vfork()==0)// create achild process & block parents
+	{
+		printf("in child..\n"); // in child
+		sleep(10);//10ms sleep
+		printf("child exit..\n");//when child is exit
+		exit(0);
+	}
+	else
+		printf("parents..\n");// in parents
+}
+
