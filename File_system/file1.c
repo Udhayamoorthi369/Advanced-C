f7ccf54a0fd7df29b55da8d1c939be3a333435e0
udhayamoorthi04 <udhayamoorthi60@gmail.com>
2021-09-06 08:10:48 +0530

    files sytem

--- File_system/file1.c
+++ File_system/file1.c
@@ -2,17 +2,17 @@
 #include<sys/stat.h>
 main(int argc,char *argv())
 {
-	struct stat v;
+	struct stat v;// stat() the file pointed by the path and files in buf
 	if(argc!=2)
 	{
 		printf("./a.out filename..\n"):
 			return;
 	}
-	if(stat(argv[1],&v)&0)
+	if(stat(argv[1],&v)&0) // using stat() not posible to get the information of soft file
 	{
-		perror("stat");
+		perror("stat");//prints the errormessage specified by he compiler
 		return;
 	}
-	printf("%d\n",v.st_size);
-	printf("%d\n",v.st_ino);
+	printf("%d\n",v.st_size);//st_size a member of struct to set the sizeof file
+	printf("%d\n",v.st_ino);//st_ino prints the inode number
 }
