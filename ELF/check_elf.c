87b48296a638aeb26aaee038bfe946c0d25f53e8
udhayamoorthi369 <udhayamoorthi651@gmail.com>
2021-09-15 14:08:23 +0530

    updates

---
+++ ELF/check_elf.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+/*! headers */
+int main(int argc,char *argv[])
+{
+if(argc<2)
+{
+printf("filename is missing..\n");
+exit(1);
+}
+int fd,data;
+
+fd=open(argv[1],O_RDONLY);
+if(fd<0)
+{
+printf("failed to open file..\n");
+exit(1);
+}
+
+read(fd,&data,sizeof(data));
+
+if(data == 0x464c457f)
+{
+printf("the file is ELF..\n");
+}
+else
+{
+printf("the file is not ELF..\n");
+}
+}
