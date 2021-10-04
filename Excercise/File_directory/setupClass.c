cd29e81b825e869d5815d681b43f57e84507e283
udhayamoorthi369 <udhayamoorthi651@gmail.com>
2021-10-04 08:45:27 +0530

    updates

---
+++ Excercise/File_directory/setupClass.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<error.h>
+
+void create_struct(char *dir,mode_t mode)
+{
+char tmp[512];
+size_t length;
+char *p;
+//snprintf(tmp, sizeof(tmp),"%s",dir);
+snprintf(tmp, sizeof(tmp),"%s",dir);    
+
+tmp[sizeof(tmp)-1]=='\0';  
+    length = strlen(tmp);
+            if(tmp[length - 1] == '/')
+                    tmp[length - 1] = '\0';
+ 
+
+for (char* p = strchr(tmp + 1, '/'); p; p = strchr(p + 1, '/')) {
+        *p = '\0';
+        if (*p == '/') {
+            if (*p != '\0') {
+                *p = '/';
+                //return -1;
+            }
+        
+        *p = '/';
+}
+
+
+
+if(access(tmp,F_OK))                            
+mkdir(tmp, S_IRWXU);
+                          *p = '/';
+}        
+                                    if(access(tmp,F_OK))
+mkdir(tmp, S_IRWXU);
+
+}
+
+
+
+int main(int argc,char* argv[])
+{
+char root[512]="23sysprog";
+struct stat s;
+int cnt;
+char buff[1024];
+if(argc>1)
+{
+strcpy(root,argv[1]);
+}
+
+if(stat(root,&s)==0){
+printf("The Directory already exist\n");
+exit(EXIT_FAILURE);
+}
+
+create_struct(root,0755);
+
+chdir(root);
+
+mkdir("test",0755);
+
+for(cnt=0;cnt<3;cnt++){
+sprintf(buff,"test/test%d",cnt+1);
+create_struct(buff,0755);
+}
+
+mkdir("program",0755);
+
+for(cnt=0;cnt<5;cnt++){
+sprintf(buff,"program/program%d",cnt+1);
+create_struct(buff,0755);
+}
+
+//mkdir("excercise",0755);
+
+for(cnt=0;cnt<10;cnt++){
+sprintf(buff,"exercise/exec%d",cnt+1);
+create_struct(buff,0755);
+}
+return 0;
+}
+/*   static void _mkdir(const char *dir) {
+            char tmp[256];
+            char *p = NULL;
+            size_t len;
+     
+            snprintf(tmp, sizeof(tmp),"%s",dir);
+            len = strlen(tmp);
+            if(tmp[len - 1] == '/')
+                    tmp[len - 1] = 0;
+            for(p = tmp + 1; *p; p++)
+                    if(*p == '/') {
+                            *p = 0;
+                            mkdir(tmp, S_IRWXU);
+                            *p = '/';
+                    }
+            mkdir(tmp, S_IRWXU);
+    }*/
---
+++ Excercise/WC /word_clw.c
@@ -0,0 +1,134 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
+
+#define MAX_LEN 1024
+
+
+void usage(char* prog)
+{
+fprintf(stderr,"usage: %s [-lcw] [filename]\n",prog);
+exit(EXIT_FAILURE);
+}
+int main(int argc,char *argv[]) {
+  /*Read the file.*/
+
+  char ch;
+
+
+int c=0;
+int l=0;
+int w=0;
+  int char_count = 0, word_count = 0, line_count = 0;
+  int in_word = 0;
+  char file_name[MAX_LEN];
+  FILE *fp=fopen("1.txt","r");
+int paramcnt;
+  //printf("Enter a file name: ");
+// scanf("%s", file_name);
+void cword()
+{
+char str[50];
+int c=0;
+int l=0;
+int w=0;
+int i=0;
+while((ch=fgetc(fp))!=-1)//,"%s",str)!=-1)
+{
+/*c++;
+if(ch == '\n' || ch == '\0')
+l++;
+*/if(ch==' ' || ch == '\t' || ch =='\n' || ch == '\0' )
+w++;
+//printf("%s\n",str);
+//count++;
+}
+if (c>0)
+{
+w++;
+l++;
+}
+printf("no of words = %d\n",w);
+
+}
+
+void l_char()
+{
+char str[50];
+int c=0;
+int l=0;
+int w=0;
+int i=0;
+while((ch=fgetc(fp))!=-1)//,"%s",str)!=-1)
+{
+;
+if(ch == '\n' || ch == '\0')
+l++;
+
+}
+if (c>0)
+{
+w++;
+l++;
+}
+printf("no of lines = %d\n",l);
+
+}
+void c_char()
+{
+char str[50];
+int c=0;
+int l=0;
+int w=0;
+int i=0;
+while((ch=fgetc(fp))!=-1)//,"%s",str)!=-1)
+{
+
+c++;
+
+}
+;
+
+printf("no of char = %d\n",c);
+
+
+}
+int i;
+while ((ch=getopt(argc,argv,"clw"))!=-1)
+{
+switch(ch)
+{
+case 'c':
+{
+c=1;
+c_char();
+
+}
+break;
+case 'l':
+{
+l=1;
+l_char();
+}
+break;
+case 'w':
+{
+w=1;
+cword();
+ 
+}
+break;
+case '?':
+{
+usage(argv[0]);
+}
+break;
+}
+}
+
+  return 0;
+}
+
+
